0ffa9b4d04710ee00139eb4f7ba984a58b2444cd
ompuno <ompuno@yahoo.fr>
2021-01-04 16:02:47 +0100

    Modif3

---
+++ docs/precache-manifest.59a160bfa1109ae3a8cbfef2bd5fcf66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48bf1c218de9fe7dbcbc",
+    "url": "/mspr2021/css/app.4ac4674c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/mspr2021/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "884a5a84b68ac9c454ad290cb17a1565",
+    "url": "/mspr2021/index.html"
+  },
+  {
+    "revision": "e38b683278123403f040",
+    "url": "/mspr2021/js/about.e9f72e9a.js"
+  },
+  {
+    "revision": "48bf1c218de9fe7dbcbc",
+    "url": "/mspr2021/js/app.7bdd6d94.js"
+  },
+  {
+    "revision": "3c0eb9c26e08545aef23",
+    "url": "/mspr2021/js/chunk-vendors.346554bc.js"
+  },
+  {
+    "revision": "76ee8578bfa7a030c4459738fae5ad09",
+    "url": "/mspr2021/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mspr2021/robots.txt"
+  }
+]);
